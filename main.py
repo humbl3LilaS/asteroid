cc34b3ba17b868171fcc4bc2e4747f80245f8725
humbl3LilaS <sabishinekobebe@gmail.com>
2025-03-13 09:46:05 +0630

    add: player and paint it in the game loop

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT,))
-    while True:
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
-        # Quit from the program
-        for evt in pygame.event.get():
-            if evt.type == pygame.QUIT:
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0,))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
